e4961cd69742c3c71a628454dac7530c97d78249
Peter Riemenschneider <peter@vmi1199568.contaboserver.net>
2024-12-02 11:23:51 +0100

    update for usermgt

--- userLib/userLib.go
+++ userLib/userLib.go
@@ -6,6 +6,13 @@ import (
 	"github.com/goccy/go-yaml"
 )
 
+type provMap map[string]string
+
+type usermgt struct {
+	list map[string]provMap
+	dbg bool
+}
+
 
 func VerifyCmd(cmdStr string) (bool) {
     cmdList := []string{"list", "get", "add", "upd", "rm"}
@@ -17,59 +24,52 @@ func VerifyCmd(cmdStr string) (bool) {
     return false
 }
 
-func VerifyApp(appStr string) (bool) {
 
-    appList := []string{"namcheap", "cloudflare", "nchsbox", "*", "all"}
-    for i:=0; i< len(appList); i++ {
-        if appStr == appList[i] {
-            return true
-        }
-    }
-    return false
-}
+func (ul *usermgt) InitUserList(yamlFil string) (err error) {
 
-type prov struct {
-	Nam string
-	Val provVal
-}
-
-type provVal struct {
-	Token string
-}
-
-type provMap map[string]string
-
-func GetList(yamlFil string) (list map[string]provMap, err error) {
-
-//	var ProvList []prov
-
-	list = make(map[string]provMap)
+	ul.list = make(map[string]provMap)
 
 	ldat, err := os.ReadFile(yamlFil)
-	if err != nil {return list, fmt.Errorf("Read List: %v", err)}
+	if err != nil {return fmt.Errorf("Read List: %v", err)}
 
-//	fmt.Printf("dbg -- %s\n", ldat)
+	err = yaml.Unmarshal(ldat,ul.list)
+	if err != nil {return fmt.Errorf("UnMarshal: %v", err)}
 
-	err = yaml.Unmarshal(ldat,&list)
-	if err != nil {return list, fmt.Errorf("UnMarshal: %v", err)}
-
-	return list, nil
+	return nil
 }
 
-func FindToken(app string, list map[string]provMap) (string, bool){
-	appVal, ok:= list[app]
+func (ul *usermgt) GetAllUsers() (users []string) {
+
+	users = make([]string, len(ul.list))
+
+	count := 0
+	for unam := range ul.list {
+    	users[count] = unam
+		count++
+	}
+
+	return users
+}
+
+
+
+
+func (ul *usermgt) GetUserToken(unam string) (string, bool){
+
+	user, ok:= ul.list[unam]
 	if !ok {return "", false}
-	
-	token, _ := appVal["token"]
+
+	token, _ := user["token"]
 
 	return token, true
 }
 
-func PrintList(list map[string]provMap) {
+func (ul *usermgt) PrintList() {
 
+	list := ul.list
 	fmt.Printf("*** api list: %d providers ***\n", len(list))
 	for nam, val := range list {
-		fmt.Printf("provider: %s\n", nam)
+		fmt.Printf("user: %s\n", nam)
 		for key, kval := range val {
 			fmt.Printf(" %s : %s\n", key, kval)
 		}
